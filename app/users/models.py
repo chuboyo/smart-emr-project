3c38f1e1a03574704ee7ff00527c1d76dc42b27e
chuboyo <akojichubiyojo1997@gmail.com>
2022-06-08 21:22:46 +0100

    created user views, templates and updated permissions in main using user boolean fields

--- app/users/models.py
+++ app/users/models.py
@@ -1,8 +1,11 @@
+from email.policy import default
 from django.contrib.auth.models import AbstractUser 
 from django.db import models
 from django.urls import reverse
+import uuid
 
 class CustomUser(AbstractUser): 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     is_doctor = models.BooleanField(blank=False, default=False)
     is_lab_staff = models.BooleanField(blank=False, default=False)
     is_clerical_staff = models.BooleanField(blank=False, default=True) # we're assuming that by default all accounts are for clerical staffs
--- app/users/views.py
+++ app/users/views.py
@@ -6,22 +6,25 @@ from django.contrib.auth import get_user_model
 
 from django.views.generic import DetailView, CreateView
 
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, DeleteView
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+from .models import CustomUser
 
 
 class UserDetailView(DetailView):
     model = get_user_model()
     context_object_name = 'user'
     template_name = 'account/user_detail.html'
+    pk_url_kwarg = 'pk'
 
 
 class UserCreateView(CreateView):
     model = get_user_model()
     template_name = 'account/new_user.html'
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('account_login')
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_superuser:
@@ -31,6 +34,13 @@ class UserCreateView(CreateView):
 
 class UserUpdateView(UpdateView):
     model = get_user_model()
-    template_name = 'user/edit_user.html'
-    fields = '__all__'
+    template_name = 'account/edit_user.html'
+    form_class = CustomUserChangeForm
+
+class UserDeleteView(DeleteView):
+    model = get_user_model()
+    template_name = 'account/delete_user.html'
+    success_url = reverse_lazy('home')
+    context_object_name = 'user'
+
 # Create your views here.
