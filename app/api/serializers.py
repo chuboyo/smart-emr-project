71cf0db1fb3204fd69151282931513e43af37449
chuboyo <akojichubiyojo1997@gmail.com>
2022-09-05 21:47:45 +0100

    pay less attentione

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -1,6 +1,6 @@
 # from dataclasses import fields
 # from pyexpat import model 
-# just a test comment
+# just a test comment ki
 from rest_framework import serializers
 
 from main.models import Patient, DoctorAppointmentHistory, LabAppointmentHistory
